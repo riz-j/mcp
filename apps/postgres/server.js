cbf7532b954777fadc59ddb7cadc7b624fb2f2d4
Rizki <adiputra979@gmail.com>
2025-04-04 12:29:34 +1100

    make the server work for postgres

--- apps/postgres/server.js
+++ apps/postgres/server.js
@@ -4,35 +4,35 @@ import { z } from "zod";
 import pg from "pg";
 
 const pool = new pg.Pool({
-  connectionString: "postgresql://admin:admin@localhost/postgres",
+	connectionString: "postgresql://admin:admin@localhost/postgres",
 });
-
-const client = await pool.connect();
-const result = await client.query("SELECT * FROM tasks");
-
-console.log(result);
 
 const server = new McpServer({
-  name: "my-postgres",
-  version: "1.0.0",
+	name: "my-postgres",
+	version: "1.0.0",
 });
 
-server.tool("list tasks",
-  {  },
-  async function() {
-    const result = await client.query("SELECT * FROM tasks");
-    const tasks = result.rows;
+server.tool("listUsers", 
+	{},
+	async () => ({
+		content: [{ type: "text", text: JSON.stringify(["Joe, Jane, James"]) }]
+	})
+);
 
-    return {
-      contents: [
-        {
-          uri: "/tasks",
-          mimeType: "application/json",
-          text: JSON.stringify(tasks),
-        }
-      ]
-    }
-  }
+server.tool("listTasks",
+	{},
+	async () => {
+		const client = await pool.connect();
+
+		const result = await client.query("SELECT * FROM tasks");
+		const tasks = result.rows;
+
+		client.release();
+		
+		return {
+			content: [{ type: "text", text: JSON.stringify(tasks) }]
+		}
+	}
 );
 
 const transport = new StdioServerTransport();
